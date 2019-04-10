7dd56407f648da3ebade8dccbc8e1df145d49695
Batoul Youssouf <batoulyoussouf@Batouls-MacBook-Pro.local>
2019-04-10 19:37:49 +0100

    Update mypythonfile.py

--- mypythonfile.py
+++ mypythonfile.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
-
-app = Flask("au-pays-des-merveilles")
-
-@app.route('/')
-
-def index():
-    return 'This is the homepage'
-
-app.run(debug=True)
+# from flask import Flask, render_template, request
+#
+# app = Flask("au-pays-des-merveilles")
+#
+# @app.route('/')
+#
+# def index():
+#     return 'This is the homepage'
+#
+# app.run(debug=True)
